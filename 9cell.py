1f183115662ef078cbb6c6e7393069c2cacdb6ee
AnTrinh2864 <118484971+AnTrinh2864@users.noreply.github.com>
2023-05-02 21:31:50 -0500

    Update 9cell.py

--- 9cell.py
+++ 9cell.py
@@ -15,16 +15,14 @@ SEV = '7'
 EIG = '8'
 NIN = '9'
 
-# (!) Try changing ALIVE to '|' and DEAD to '-'.
 
 # The cells and nextCells are dictionaries for the state of the game.
 # Their keys are (x, y) tuples and their values are one of the ALIVE
 # or DEAD values.
 nextCells = {}
-# Put random dead and alive cells into nextCells:
+
 for x in range(WIDTH):  # Loop over every possible column.
     for y in range(HEIGHT):  # Loop over every possible row.
-         # 50/50 chance for starting cells being alive or dead.
          if random.randint(0, 8) == 0:
              nextCells[(x, y)] = NIN  # Add a living cell.
          elif random.randint(0, 8) == 1:
@@ -222,6 +220,5 @@ while True:  # Main program loop.
      try:
          time.sleep(1)  # Add a 1 second pause to reduce flickering.
      except KeyboardInterrupt:
-         print("Conway's Game of Life")
-         print('By Al Sweigart al@inventwithpython.com')
+         print("9-Cell ruleset")
          sys.exit()  # When Ctrl-C is pressed, end the program.
